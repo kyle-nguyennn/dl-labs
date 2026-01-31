A1: 2-layer NN - fix 2nd layer no sigmoid and gradient of b

assignment1/models/softmax_regression.py
@@ -68,11 +68,11 @@ class SoftmaxRegression(_baseNetwork):
         #   Store your intermediate outputs before ReLU for backwards               #
         #############################################################################
 
-        print(f"X shape: {X.shape}")
-        print(f"y shape: {y.shape}")
+        # print(f"X shape: {X.shape}")
+        # print(f"y shape: {y.shape}")
         W = self.weights['W1']
         n = y.shape[0]
-        print(f"N = {n}")
+        # print(f"N = {n}")
         z = np.matmul(X, W)
         a = self.ReLU(z)
         p = self.softmax(a)
@@ -95,12 +95,12 @@ class SoftmaxRegression(_baseNetwork):
         y_onehot = self._onehot(y)
         _a = (p - y_onehot)/n # uptream backward gradient at a
         a_prime = self.ReLU_dev(z)
-        print(f"_a shape: {_a.shape}")
-        print(f"ReLU' shape: {a_prime.shape}")
+        # print(f"_a shape: {_a.shape}")
+        # print(f"ReLU' shape: {a_prime.shape}")
         _z = _a * a_prime # element wise multiplication
         _W = X.T @ _z
         self.gradients['W1'] = gradient = _W
-        print(f"Upstream grad a W: {gradient}")
+        # print(f"Upstream grad a W: {gradient}")
 
         #############################################################################
         #                              END OF YOUR CODE                             #

assignment1/models/two_layer_nn.py
@@ -94,8 +94,7 @@ class TwoLayerNet(_baseNetwork):
         z1 = X @ W1 + b1
         a1 = self.sigmoid(z1)
         z2 = a1 @ W2 + b2
-        a2 = self.sigmoid(z2)
-        p = self.softmax(a2)
+        p = self.softmax(z2)
 
         loss = self.cross_entropy_loss(p, y)
         accuracy = self.compute_accuracy(p, y)
@@ -117,14 +116,13 @@ class TwoLayerNet(_baseNetwork):
 
         n = X.shape[0] 
         y_onehot = self._onehot(y)
-        _a2 = 1/n * (p - y_onehot) # dl/da2
-        _z2 = _a2 * self.sigmoid_dev(z2) # element-wise product, as sigmoid is an element-wise function
+        _z2 = 1/n * (p - y_onehot) # dl/da2
         _W2 = a1.T @ _z2 # dl/dw2
-        _b2 = np.ones(self.num_classes)
+        _b2 = _z2.sum(axis=0)
         _a1 = _z2 @ W2.T # dl/da1
         _z1 = _a1 * self.sigmoid_dev(z1)
         _W1 = X.T @ _z1 # dl/dw1
-        _b1 = np.ones(self.hidden_size)
+        _b1 = _z1.sum(axis=0)
 
         self.gradients['W1'] = _W1
         self.gradients['b1'] = _b1