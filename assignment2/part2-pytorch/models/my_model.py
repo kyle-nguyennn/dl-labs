cb34846ccc191787310acb5274fbab355dc0ba4e
Kyle Nguyen <nghia002@outlook.com>
2026-02-24 21:50:50 +0800

    A2 - part 2: my model improved

--- assignment2/part2-pytorch/models/my_model.py
+++ assignment2/part2-pytorch/models/my_model.py
@@ -34,20 +34,25 @@ def hello_do_you_copy():
 class ResidualBlock(nn.Module):
     def __init__(self, in_channels, out_channels, stride=1):
         super().__init__()
-        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=stride, padding=1)
+        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=stride, padding=1, bias=False)
         self.bn1 = nn.BatchNorm2d(out_channels)
-        self.relu1 = nn.ReLU(inplace=True)
-        self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=1, padding=1)
+        self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=1, padding=1, bias=False)
         self.bn2 = nn.BatchNorm2d(out_channels)
-        self.relu2 = nn.ReLU(inplace=True)
-        self.conv3 = nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=1, padding=1)
-        self.bn3 = nn.BatchNorm2d(out_channels)
-        self.relu3 = nn.ReLU(inplace=True)
+        self.relu = nn.ReLU(inplace=True)
+
+        # Shortcut connection to match dimensions
+        self.shortcut = nn.Sequential()
+        if stride != 1 or in_channels != out_channels:
+            self.shortcut = nn.Sequential(
+                nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(out_channels)
+            )
 
     def forward(self, x):
-        out = self.relu1(self.bn1(self.conv1(x)))
-        out = self.relu2(self.bn2(self.conv2(out)))
-        out = self.relu3(self.bn3(self.conv3(out)))
+        out = self.relu(self.bn1(self.conv1(x)))
+        out = self.bn2(self.conv2(out))
+        out += self.shortcut(x)  # skip connection
+        out = self.relu(out)
         return out
 
 class MyModel(nn.Module):
@@ -56,14 +61,15 @@ class MyModel(nn.Module):
         #############################################################################
         # TODO: Initialize the network weights                                      #
         #############################################################################
-        self.conv1 = nn.Conv2d(in_channels=3, out_channels=64, kernel_size=3, stride=1, padding=1)
+        self.conv1 = nn.Conv2d(in_channels=3, out_channels=64, kernel_size=3, stride=1, padding=1, bias=False)
         self.bn1 = nn.BatchNorm2d(64)
-        self.layer1 = ResidualBlock(64, 128, stride=2)
-        self.layer2 = ResidualBlock(128, 256, stride=2)
-        self.layer3 = ResidualBlock(256, 512, stride=2)
+        self.relu = nn.ReLU(inplace=True)
+        self.layer1 = ResidualBlock(64, 64, stride=1)
+        self.layer2 = ResidualBlock(64, 128, stride=2)
+        self.layer3 = ResidualBlock(128, 256, stride=2)
+        self.layer4 = ResidualBlock(256, 512, stride=2)
         self.pool = nn.AdaptiveAvgPool2d((1, 1))
-        # self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
-        # self.dropout = nn.Dropout(p=0.5)
+        self.dropout = nn.Dropout(p=0.25)
         self.fc = nn.Linear(512, 10)
         #############################################################################
         #                              END OF YOUR CODE                             #
@@ -74,14 +80,14 @@ class MyModel(nn.Module):
         #############################################################################
         # TODO: Implement forward pass of the network                               #
         #############################################################################
-        out = self.conv1(x)
-        out = self.bn1(out)
+        out = self.relu(self.bn1(self.conv1(x)))
         out = self.layer1(out)
         out = self.layer2(out)
         out = self.layer3(out)
+        out = self.layer4(out)
         out = self.pool(out)
         out = out.view(out.size(0), -1)
-        # out = self.dropout(out)
+        out = self.dropout(out)
         outs = self.fc(out)
         #############################################################################
         #                              END OF YOUR CODE                             #
